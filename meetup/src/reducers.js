42e510ee7fda0daf4af58bd89cfe577f8a1854c3
Joungwoo Baik <joungwoo.baik@gmail.com>
2020-01-26 00:57:04 -0800

    Remove reducers

--- meetup/src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { } from './app/features/event/EventDashBoard/eventDashboardActions.js'
-
-const rootReducer = combineReducers({
-
-})
-
-export default rootReducer
